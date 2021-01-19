dc8e45559dcc2ef09ac0587fe464d3667a7f7d5b
NekoDevelopper <nekodevelopper@gmail.com>
2021-01-20 00:25:57 +0300

    add Documentation

--- src/Modules/Mqtt/OpenHouseMqtt.h
+++ src/Modules/Mqtt/OpenHouseMqtt.h
@@ -6,26 +6,26 @@
 #define OHMqttHandlersCount 10
 #endif
 
-class OHMqttSender
+class OHMqttSender //Sender Class
 {
     private:
-        char* topic;
+        char* topic; // Topic
     public:
-        OHMqttSender(char *Topic);
-        void send(char *message);
+        OHMqttSender(char *Topic); // Class Constructor
+        void send(char *message); //Send func
 
 };
-class OHMqttHandler
+class OHMqttHandler //Handler Class
 {
     public:
-        OHMqttHandler(char *Topic, void (*Func)(char *message));
-        void (*func)(char *mesage);
-        char *topic;
+        OHMqttHandler(char *Topic, void (*Func)(char *message)); //Class Constructor
+        void (*func)(char *mesage); // Function, called message recived from it's topic
+        char *topic; //Topic to subscribe
 };
 
 class OHMqttClass{
     private:
-        static void CallbackFunc(char* topic, byte* payload, unsigned int length);
+        static void CallbackFunc(char* topic, byte* payload, unsigned int length); //Callback func
         char *mqttClientId;
         int *mqttPort;
         char *mqttServer;
@@ -40,7 +40,5 @@ class OHMqttClass{
         void Loop();
         void Publish(char *topic, char *message);
         void Subscribe(char *topic);
-        void AddReciver(char *topic, void (*func)(char *message));
-        OHMqttSender AddSender(char *topic);
 };
 extern OHMqttClass OHMqtt;
--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,45 @@
 #include "Modules/Mqtt/OpenHouseMqtt.h"
 #include <DHT.h>
 
-byte ethernetMac[6] = {0x5A,0x12,0xF0,0xAF,0xAD,0x71};
-char MqttServerAdress[] = "192.168.1.14";
-char MqttClientId[] = "Test";
-int MqttServerPort = 1883;
+byte ethernetMac[6] = {0x5A,0x12,0xF0,0xAF,0xAD,0x71}; // Mac Adress 
+char MqttServerAdress[] = "192.168.1.14"; // Mqtt ip adress
+char MqttClientId[] = "Test"; // MQTT Client ID
+int MqttServerPort = 1883; // MQTT Server Port
 
-DHT dht(3, 22);
-OHMqttSender HumiditySender((char*)"/Test/Humidity");
-OHMqttSender TemperatureSender((char*)"/Test/Temperature");
+DHT dht(3, 22); // Create DHT Sersor object
+OHMqttSender HumiditySender((char*)"/Test/Humidity"); // Create MQTT Sender Object
+OHMqttSender TemperatureSender((char*)"/Test/Temperature"); // Create MQTT Sender Object
 
-void RelayFunc(char *message){digitalWrite(2,atoi(message));}
-OHMqttHandler Relay((char*)"/Test/Relay",&RelayFunc);
+void RelayFunc(char *message){digitalWrite(2,atoi(message));} // Create Relay Switch Function
+OHMqttHandler Relay((char*)"/Test/Relay",&RelayFunc); // Create MQTT Handler
 
 void setup()
 {
-    Serial.begin(9600);
-    dht.begin();
-    pinMode(2, OUTPUT);
-    OHMqtt.Begin(MqttServerAdress, &MqttServerPort, MqttClientId);
-    OHNet.Begin(ethernetMac);
+    Serial.begin(9600); //Begining Arduino Serial Interface
+    dht.begin(); //Begining DHT sensor
+    pinMode(2, OUTPUT); //Configure Hardware pin 2 to OUTPUT
+    OHMqtt.Begin(MqttServerAdress, &MqttServerPort, MqttClientId); //Begining MQTT
+    OHNet.Begin(ethernetMac); //Begining Ethernet
 }
 
-unsigned long int millissss = 0;
-float Temperature;
-float Humidity;
+unsigned long int millissss = 0; //Create Timer property
+float Temperature; //Create Temperature property
+float Humidity;//Create Humidity property
 
 void loop()
 {
-    OHNet.Loop();
-    OHMqtt.Loop();
+    OHNet.Loop(); // Call Ethernet loop function
+    OHMqtt.Loop(); // Call MQTT loop function
 
     if (millis() - millissss >= 5000)
     {
-        Temperature = dht.readTemperature();
-        Humidity = dht.readHumidity();
-        char buf[20];
-        dtostrf(Temperature, 3, 3, buf);
-        TemperatureSender.send(buf);
-        dtostrf(Humidity, 3, 3, buf);
-        HumiditySender.send(buf);
+        Temperature = dht.readTemperature(); //Read temperature from DHT sensor
+        Humidity = dht.readHumidity(); //Read Humidity from DHT Sensor
+        char buf[20]; 
+        dtostrf(Temperature, 3, 3, buf); // Float-> Char
+        TemperatureSender.send(buf); // Send Temperature 
+        dtostrf(Humidity, 3, 3, buf); // Float-> Char
+        HumiditySender.send(buf); //Send Humidity
         millissss = millis();
     }
     else if (millis() - millissss < 0)
